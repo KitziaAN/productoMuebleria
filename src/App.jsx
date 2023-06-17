f7a6be3b532b27f966073919b8bca6a83855b2a3
KitziaAN <utp0144170@alumno.utpuebla.edu.mx>
2023-06-17 17:30:29 -0600

    Integración de mostrar usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Content from './components/Content';
+import EyScontent from './components/EyScontent';
+import Procontent from './components/Procontent';
+import Usucontent from './components/Usucontent';
+import Graficas from './components/Graficas';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Content/>} /> 
+        <Route path='/EyS' element={<EyScontent />} />
+        <Route path='/Productos' element={<Procontent/>} />
+        <Route path='/Usuarios' element={<Usucontent/>} />
+        <Route path='/Graficas' element={<Graficas/>} />
+
+
+      </Routes>
+     </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
