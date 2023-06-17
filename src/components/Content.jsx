d5f814acc14b5f0a4d8902fcaee9941337eb23ff
Brisa <utp0146832@alumno.utpuebla.edu.mx>
2023-06-17 16:06:14 -0600

    Graficas de entredas y salidas

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Categorias from './Rutas/Categorias';
+const Content = () => {
+  return (
+    <>
+     <Navbar/>
+     <Categorias />
+     <Footer/>
+    </>
+    
+  );
+};
+
+export default Content;
---
+++ src/components/Graficas.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Navbar from './Navbar';
+import GraficasEyS from './Rutas/GraficasEyS';
+
+const Graficas = () => {
+  return (
+    <>
+     <Navbar/>
+     <GraficasEyS/>
+     <Footer/>
+    </>
+  );
+};
+
+export default Graficas
