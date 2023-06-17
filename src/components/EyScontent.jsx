daa9f7493396f4dd3d6e6e99222bea75cea3d7f7
Brisa <utp0146832@alumno.utpuebla.edu.mx>
2023-06-17 16:12:14 -0600

    Integración de rutas

---
+++ src/components/EyScontent.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Navbar from './Navbar';
+import EntradasySalidas from './Rutas/EntradasySalidas';
+
+const Categoriascon = () => {
+  return (
+    <>
+     <Navbar/>
+     <EntradasySalidas/>
+     <Footer/>
+    </>
+  );
+};
+
+export default Categoriascon
---
+++ src/components/Usucontent.jsx
@@ -0,0 +1,17 @@
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Productos from './Rutas/Productos';
+
+
+const Procontent = () => {
+  return (
+    <>
+        <Navbar/>
+        <Productos/>
+        <Footer/>
+
+    </>
+  )
+}
+
+export default Procontent
