24cf037f93cd2252a9b8409927a6bae5ceffd2da
Erick <utp0139196@alumno.utpuebla.edu.mx>
2023-08-05 23:03:09 -0600

    cambio de diseño de footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <>
-      <footer style={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: 'black' }} className="main-footer">
-        <strong> Desarrollo Web Integral 9° A </strong>
-        <div className="float-right d-none d-sm-inline">
-          M.I Paulo Daniel Vazquez Mora
-        </div>
-      </footer>
-    </>
+    <footer style={styles.footer}>
+      <div style={styles.container}>
+        <strong>Desarrollo Web Integral 9° A</strong>
+        <div>M.I Paulo Daniel Vazquez Mora</div>
+      </div>
+    </footer>
   );
 }
+
+const styles = {
+  footer: {
+    backgroundColor: 'black',
+    color: 'white',
+    textAlign: 'center',
+    padding: '10px 0',
+    marginTop: '20px', // Espacio en blanco arriba del footer
+  },
+  container: {
+    maxWidth: '1200px',
+    margin: '0 auto',
+  },
+};
 
 export default Footer;
